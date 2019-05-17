3cf1a2c129234f2248143995e2e473f36a7fe841
Kognise <felix.mattick@gmail.com>
2019-05-16 20:40:24 -0400

    Add actual functionality

--- index.js
+++ index.js
@@ -10,37 +10,39 @@ client.on('ready', async () => {
   const infoChannel = guild.channels.get('559771387766505472')
   const messageChannel = guild.channels.get('572967189053702163')
 
-  const member = guild.members.get('389576827754250241')
-  // wanderingRole.members.forEach(async ({ displayName, joinedTimestamp }) => {
-    const timeSinceJoin = Date.now() - member.joinedTimestamp
-    const daysSinceJoin = Math.floor(timeSinceJoin / 1000 / 60 / 60 / 24)
-    const daysLeft = 7 - daysSinceJoin
+  setInterval(async () => {
+    console.log('> Checking users')
+    wanderingRole.members.forEach(async (member) => {
+      const timeSinceJoin = Date.now() - member.joinedTimestamp
+      const daysSinceJoin = Math.floor(timeSinceJoin / 1000 / 60 / 60 / 24)
+      const daysLeft = 7 - daysSinceJoin
 
-    let message = ''
-    message += member.toString()
-    if (daysLeft > 1) {
-      message += ` you have **${daysLeft} days** left to send your verification message! See ${infoChannel} to learn how`
-    } else if (daysLeft === 1) {
-      try {
-        await member.send(`Just a friendly reminder, you only have **1 day** left to become verified on PwnSquad or you'll be **kicked**! *Don't worry, all you have to do is tell us a little about yourself :)*`)
-      } catch(error) {
-        console.log(`> Couldn't send a DM to ${member.displayName}`)
-      }
-      message += ` you have **1 day** left to send your verification message, and you're really close to being **kicked**. See ${infoChannel} to learn how`
-    } else {
-      if (member.kickable) {
-        await member.kick()
+      let message = ''
+      message += member.toString()
+      if (daysLeft > 1) {
+        message += ` you have **${daysLeft} days** left to send your verification message! See ${infoChannel} to learn how`
+      } else if (daysLeft === 1) {
         try {
-          await member.send(`Sorry, but you've kicked from PwnSquad for not sending your verification message fast enough. In our defense, we warned you ahread of time! *If you want to try again feel free to join back*`)
+          await member.send(`Just a friendly reminder, you only have **1 day** left to become verified on PwnSquad or you'll be **kicked**! *Don't worry, all you have to do is tell us a little about yourself :)*`)
         } catch(error) {
           console.log(`> Couldn't send a DM to ${member.displayName}`)
         }
-        return
+        message += ` you have **1 day** left to send your verification message, and you're really close to being **kicked**. See ${infoChannel} to learn how`
+      } else {
+        if (member.kickable) {
+          await member.kick()
+          try {
+            await member.send(`Sorry, but you've kicked from PwnSquad for not sending your verification message fast enough. In our defense, we warned you ahread of time! *If you want to try again feel free to join back*`)
+          } catch(error) {
+            console.log(`> Couldn't send a DM to ${member.displayName}`)
+          }
+          return
+        }
+        message += ` should've been kicked, but I somehow failed. ${superRole} halp!!!`
       }
-      message += ` should've been kicked, but I somehow failed. ${superRole} halp!!!`
-    }
-    await messageChannel.send(message)
-  // })
+      await messageChannel.send(message)
+    })
+  }, 24 * 60 * 60 * 1000)
 })
 
 client.login(process.env.TOKEN)
