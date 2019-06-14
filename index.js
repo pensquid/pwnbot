d0a9576b14af842458169fd3b8acd060c748d6fa
Joshua Martinez <joshkmartinez@gmail.com>
2019-06-14 10:31:26 -0700

    improve nude filter

--- index.js
+++ index.js
@@ -1,92 +1,191 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const Discord = require('discord.js')
-const cron = require('node-cron')
-const client = new Discord.Client()
-const prefix = ";;"; // TODO: Add config file?
+const Discord = require("discord.js");
+const cron = require("node-cron");
+const client = new Discord.Client();
+const prefix = ";;";
 
-client.on('ready', async () => {
-  console.log(`> Logged in as ${client.user.tag}`)
-  client.user.setActivity('unverified members', { type: 'WATCHING' })
+const loaded = {
+  guild: null,
+  roles: {
+    super: null,
+    wandering: null,
+    verified: null
+  },
+  channels: {
+    info: null,
+    countdown: null,
+    lobby: null,
+    roles: null,
+    challenges: null,
+    rules: null
+  },
+  emojis: {
+    no: null,
+    yes: null
+  },
+  members: {
+    ctfbot: null
+  }
+};
+const welcomeEmojis = ["😝", "🍻", "😄", "🎉", "👍"];
 
-  const guild = client.guilds.get('520444262685474816')
-  const superRole = guild.roles.get('520452795250507795')
-  const infoChannel = guild.channels.get('559771387766505472')
-  const messageChannel = guild.channels.get('572967189053702163')
+const welcome = async member => {
+  await loaded.channels.lobby.send(
+    `
+${member} welcome to PwnSquad! Make sure to read the ${
+      loaded.channels.rules
+    } and get some cool ${loaded.channels.roles}.
+
+If you're interested:
+- We have some fun challenges going on in ${loaded.channels.challenges}
+- If you're into CTFs check out ${loaded.members.ctfbot}
+  `.trim()
+  );
+  await loaded.channels.lobby.send(
+    welcomeEmojis[Math.floor(Math.random() * welcomeEmojis.length)]
+  );
+};
+
+client.on("ready", async () => {
+  console.log(`> Logged in as ${client.user.tag}`);
+  client.user.setActivity("unverified members", { type: "WATCHING" });
+
+  loaded.guild = client.guilds.get("520444262685474816");
+
+  loaded.roles.super = loaded.guild.roles.get("520452795250507795");
+  loaded.roles.wandering = loaded.guild.roles.get("563143582765023232");
+  loaded.roles.verified = loaded.guild.roles.get("520461398183247875");
+
+  loaded.channels.info = loaded.guild.channels.get("559771387766505472");
+  loaded.channels.countdown = loaded.guild.channels.get("572967189053702163");
+  loaded.channels.lobby = loaded.guild.channels.get("520452924967747584");
+  loaded.channels.roles = loaded.guild.channels.get("520466535198752778");
+  loaded.channels.challenges = loaded.guild.channels.get("585583470626078720");
+  loaded.channels.rules = loaded.guild.channels.get("540370552200757248");
+
+  loaded.emojis.no = loaded.guild.emojis.get("540601942385229834");
+  loaded.emojis.yes = loaded.guild.emojis.get("540601942217457674");
+
+  loaded.members.ctfbot = loaded.guild.members.get("580257069760905216");
 
   const task = async () => {
-    console.log('> Checking users')
+    console.log("> Checking users");
 
-    const newGuild = await guild.fetchMembers()
-    const wanderingRole = newGuild.roles.get('563143582765023232')
+    const newGuild = await loaded.guild.fetchMembers();
+    const freshWanderingRole = newGuild.roles.get(loaded.roles.wandering.id);
 
-    wanderingRole.members.forEach(async (member) => {
-      const timeSinceJoin = Date.now() - member.joinedTimestamp
-      const daysSinceJoin = Math.floor(timeSinceJoin / 1000 / 60 / 60 / 24)
-      const daysLeft = 7 - daysSinceJoin
-      if (daysLeft >= 7) return
+    freshWanderingRole.members.forEach(async member => {
+      const timeSinceJoin = Date.now() - member.joinedTimestamp;
+      const daysSinceJoin = Math.floor(timeSinceJoin / 1000 / 60 / 60 / 24);
+      const daysLeft = 7 - daysSinceJoin;
+      if (daysLeft >= 7) return;
 
-      let message = ''
-      message += member.toString()
+      let message = "";
+      message += member.toString();
       if (daysLeft > 1) {
-        message += ` you have **${daysLeft} days** left to send your verification message! See ${infoChannel} to learn how`
+        message += ` you have **${daysLeft} days** left to send your verification message! See ${
+          loaded.channels.info
+        } to learn how`;
       } else if (daysLeft === 1) {
         try {
-          await member.send(`Just a friendly reminder, you only have **1 day** left to become verified on PwnSquad or you'll be **kicked**! *Don't worry, all you have to do is tell us a little about yourself :)*`)
-        } catch(error) {
-          console.log(`> Couldn't send a DM to ${member.displayName}`)
+          await member.send(
+            `Just a friendly reminder, you only have **1 day** left to become verified on PwnSquad or you'll be **kicked**! *Don't worry, all you have to do is tell us a little about yourself :)*`
+          );
+        } catch (error) {
+          console.log(`> Couldn't send a DM to ${member.displayName}`);
         }
-        message += ` you have **1 day** left to send your verification message, and you're really close to being **kicked**. See ${infoChannel} to learn more`
+        message += ` you have **1 day** left to send your verification message, and you're really close to being **kicked**. See ${
+          loaded.channels.info
+        } to learn more`;
       } else {
-        if (member.kickable) {
-          await member.kick()
-          try {
-            await member.send(`Sorry, but you've kicked from PwnSquad for not sending your verification message fast enough. In our defense, we warned you ahread of time! *If you want to try again feel free to join back*`)
-          } catch(error) {
-            console.log(`> Couldn't send a DM to ${member.displayName}`)
-          }
-          return
+        await member.kick();
+        try {
+          await member.send(
+            `Sorry, but you've kicked from PwnSquad for not sending your verification message fast enough. In our defense, we warned you ahread of time! *If you want to try again feel free to join back*`
+          );
+        } catch (error) {
+          console.log(`> Couldn't send a DM to ${member.displayName}`);
         }
-        message += ` should've been kicked, but I somehow failed. ${superRole} halp!!!`
+        return;
       }
-      await messageChannel.send(message)
-    })
-  }
+      await loaded.channels.countdown.send(message);
+    });
+  };
 
-  cron.schedule('0 8 * * *', task)
-})
+  cron.schedule("0 8 * * *", task);
+});
 
 client.on("message", async message => {
-  // TODO: check if not a super user
-  const guild = client.guilds.get('520444262685474816')
-  const superRole = guild.roles.get('520452795250507795')
-  const verifedRole = guild.roles.get("520461398183247875");
-  const wanderingRole = guild.roles.get("563143582765023232");
-  if (message.author.bot) return;
-  if (message.content.indexOf(prefix) !== 0) return;
+  if (
+    message.member.roles.get(loaded.roles.wandering.id) &&
+    (message.content.includes("81ZH2Y") ||
+      message.content.includes("nakedphotos.club") ||
+      message.content.includes("privatepage.vip") ||
+      message.content.includes("18+ photos"))
+  ) {
+    await message.delete();
+    await message.member.ban("Nudes");
+    return;
+  }
 
-  const args = message.content
-    .slice(prefix)
-    .trim()
-    .split(/ +/g);
-  const command = args
-    .shift()
-    .toLowerCase()
-    .substring(prefix.length);
-  console.log(command);
-  console.log(args);
-  if (command === "verify") {
-    message.mentions.members.forEach(async member => {
-      message.channel.send(
-        "Congratulations" + member.toString() + ", You have been verified!"
+  if (!message.content.startsWith(prefix)) return;
+  if (!message.member.roles.get(loaded.roles.super.id)) return;
+
+  const members = message.mentions.members;
+
+  if (message.content.startsWith(`${prefix}verify`)) {
+    if (members.size < 1) {
+      await message.channel.send(
+        `${loaded.emojis.no} You must specify a member to verify!`
       );
-      member
-        .addRole(verifedRole)
-        .then(message.channel.send(":white_check_mark:"))
-        .catch(console.error);
-      member.removeRole(wanderingRole).catch(console.error);
-    });
+      return;
+    }
+
+    for (let [_, member] of members) {
+      if (member.roles.get(loaded.roles.verified.id)) {
+        await message.channel.send(
+          `${loaded.emojis.no} ${member} is already verified!`
+        );
+      } else {
+        await member.removeRole(loaded.roles.wandering);
+        await member.addRole(loaded.roles.verified);
+        await message.channel.send(
+          `${loaded.emojis.yes} ${member} has been verified.`
+        );
+        await welcome(member);
+      }
+    }
+  } else if (message.content.startsWith(`${prefix}reject`)) {
+    if (members.size < 1) {
+      await message.channel.send(
+        `${loaded.emojis.no} You must specify a member to reject!`
+      );
+      return;
+    }
+
+    for (let [_, member] of members) {
+      if (member.roles.get(loaded.roles.verified.id)) {
+        await message.channel.send(
+          `${
+            loaded.emojis.no
+          } ${member} is verified, so you can't reject them! You may want to kick them instead.`
+        );
+      } else {
+        try {
+          await member.send(
+            "Sorry, but your application has been rejected. If you think you can do better fill out the form again. "
+          );
+        } catch (error) {
+          console.log(`> Couldn't send a DM to ${member.displayName}`);
+        }
+        await member.kick();
+        await message.channel.send(
+          `${loaded.emojis.yes} ${member} has been rejected.`
+        );
+      }
+    }
   }
 });
 
-client.login(process.env.BOT_TOKEN)
+client.login(process.env.BOT_TOKEN);
