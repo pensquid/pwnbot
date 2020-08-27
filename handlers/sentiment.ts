be638fc7aa7ee8346a04f3b375501946b4902266
Felix <felix.mattick@gmail.com>
2020-08-27 01:40:01 -0400

    lincc

--- handlers/sentiment.ts
+++ handlers/sentiment.ts
@@ -18,7 +18,10 @@ export class SentimentHandler extends BaseHandler {
     await this.loaded.channels.reports.send(`
 **This message by ${message.member} in ${message.channel} has been detected as potentially toxic:**
 ${message.content}
-*These toxicity predictions are wildly accurate so no action has been taken. Toxicity score: ${toxicity}*
+
+These toxicity predictions are wildly accurate so no action has been taken
+Toxicity score: ${toxicity}
+Message link: https://discordapp.com/channels/${message.guild.id}/${message.channel.id}/${message.id}
     `.trim())
 
     return true
